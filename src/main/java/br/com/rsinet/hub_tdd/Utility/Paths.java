6d47bd7bfab70f0d524966c5413838dbf06fb342
Renato Grasso Bueno Nascimento <renato.nascimento@rsinet.com.br>
2020-01-28 16:12:35 -0300

    Atualizada a massa de dados

--- src/main/java/br/com/rsinet/hub_tdd/Utility/Paths.java
+++ src/main/java/br/com/rsinet/hub_tdd/Utility/Paths.java
@@ -4,9 +4,9 @@ public class Paths {
 
 
 
-		public static final String Path_cadastro = "C:\\Users\\renato.nascimento\\eclipse-workspace\\TestDomain\\AvaliacaoTDD-master\\src\\main\\java\\br\\com\\rsinet\\hub_tdd\\Utility\\";
+		public static final String Path_cadastro = "C:\\Users\\renato.nascimento\\eclipse-workspace\\Avaliacao1\\advantageShopping\\src\\main\\java\\br\\com\\rsinet\\hub_tdd\\Utility\\";
 
-		public static final String File_cadastro = "Massa.xlsx";
+		public static final String File_cadastro = "MassaDados.xlsx";
 
 	}
 
